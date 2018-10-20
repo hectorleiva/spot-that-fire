ed70db847c15c11792700a31f3b1ef93f4bf692e
Hector Leiva <hector@hectorleiva.com>
2018-10-20 12:37:41 -0400

    adding webcam, able to capture images to base64

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Webcam from "react-webcam";
+import {isMobile} from 'react-device-detect';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.webcam = null;
+    this.state = {
+      imageSrc: '',
+      error: ''
+    };
+
+    this.onCapture = this.onCapture.bind(this);
+  }
+
+  onCapture = () => {
+    const imageSrc = this.webcam.getScreenshot();
+    console.log('captured: ', imageSrc);
+    this.setState({
+      imageSrc
+    });
+  }
+
+  reportError(t) {
+    console.error(t);
+    this.setState({
+      error: t
+    });
+  }
+
   render() {
+    const videoConstraints = {
+      width: 1280,
+      height: 720,
+      facingMode: isMobile ? { exact: "environment" } : "user"
+    };
+    console.log('imageSrc: ', this.state.imageSrc);
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+          <button onClick={this.onCapture}>
+          <img src={logo} className="App-logo" alt="logo" />
+            Click Here to take an image
+          </button>
           </a>
         </header>
+        <div>
+          {this.state.imageSrc ? 
+            <img src={this.state.imageSrc} /> :
+            null
+          }
+        </div>
+        <div>
+          Error Reporting: {this.state.error.message}
+        </div>
+        <div>
+          <Webcam
+            audio={false}
+            height={350}
+            ref={(ref) => { this.webcam = ref; }}
+            screenshotFormat="image/jpeg"
+            width={350}
+            onUserMediaError={(t) => this.reportError(t)}
+            videoConstraints={videoConstraints}
+          />
+        </div>
       </div>
     );
   }
